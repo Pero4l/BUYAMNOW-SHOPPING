4a5f18901e10d0032fba7f32b2910b1b26f65df4
Peter Abahim <lilpero03@gmail.com>
2025-07-30 00:00:47 +0100

    feature i made my logo a link back to home

--- src/Components/Layout/Narbar.jsx
+++ src/Components/Layout/Narbar.jsx
@@ -5,8 +5,17 @@ import { VscSettings } from "react-icons/vsc";
 import { CiSearch } from "react-icons/ci";
 import { TbMenuDeep } from "react-icons/tb";
 import { Link } from "react-router";
+import { useNavigate } from "react-router";
 
 const Navbar = () => {
+
+  const navigate = useNavigate();
+  const handleHomeClick = () => {
+    navigate("/");
+  };
+  const handleShopClick = () => {
+    navigate("/shop");
+  };
   return (
     <>
       <div>
@@ -24,14 +33,12 @@ const Navbar = () => {
             </li>
           </ul>
 
-         <Link to='/'>
-         <h1 className="lg:text-3xl text-2xl text-black w-74 font-semibold lg:p-5 flex gap-[0.5px] lg:gap-2">
+          <h1 onClick={handleHomeClick} className="lg:text-3xl text-2xl text-black w-74 font-semibold lg:p-5 flex gap-[0.5px] lg:gap-2">
             BuyAmNow
             <span className="border border-green-950 lg:size-5 size-4 flex justify-center items-center text-xs lg:text-sm rounded-full ">
               R
             </span>
           </h1>
-         </Link>
 
           <div className="flex justify-center items-center gap-x-3">
             <button>
